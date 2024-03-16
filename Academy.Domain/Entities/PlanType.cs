484ba8bf01c250fd31a01a4f039b5660d89fb423
Rafael Nascimento <rafaelnd2001@gmail.com>
2024-03-15 21:52:37 -0300

    Adding property for period in plan type entity

--- Academy.Domain/Entities/PlanType.cs
+++ Academy.Domain/Entities/PlanType.cs
@@ -2,19 +2,22 @@ namespace Academy.Domain.Entities
 {
     public class PlanType : Entity
     {
-        public PlanType(string name)
+        public PlanType(string name, int period)
         {
             Name = name;
+            Period = period;
         }
         
-        public PlanType(int id, string name)
+        public PlanType(int id, string name, int period)
         {
             Id = id;
             Name = name;
+            Period = period;
         }
 
         public string Name { get; private set; }
-        
+        public int Period { get; private set; }
+
         public IEnumerable<Plan> Plans { get; set; } = new List<Plan>();
     }
 }
